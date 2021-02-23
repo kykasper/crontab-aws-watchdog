a83d7261984aeb15082122686bfdd8112333738c
kykasper <kasper.6626@gmail.com>
2021-02-23 10:39:55 +0900

    add: cronjob.sh

---
+++ cronjob.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -
+# You must set 'LOGFILE'
+readonly PROCNAME=${0##*/}
+function log() {
+  local fname=${BASH_SOURCE[1]##*/}
+  echo -e "$(date '+%Y-%m-%dT%H:%M:%S') ${PROCNAME} (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $@" | tee -a ${LOGFILE}
+}
